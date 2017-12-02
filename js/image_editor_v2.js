88beb195be02d2cc638b39c1ff1477d417547ab9
shamelessApathy <bcm811@gmail.com>
2017-12-02 15:46:36 -0700

    able to create a Canvas class with prototypes, been able to manipulate the canvas into being resized via html5 interactions and js listneerrs

--- js/image_editor_v2.js
+++ js/image_editor_v2.js
@@ -1,6 +1,15 @@
 'use strict';
 
 
+
+
+
+
+
+// All actions after this are for when page is finished loading
+$(function(){
+
+
 /**
 * Canvas Class
 * @constructor
@@ -13,6 +22,7 @@ function Canvas(id, x, y)
 {
 	this.id = id;
 	this.jstring = "#"+this.id;
+	this.element = document.getElementById('ie-canvas');
 	this.setLocation(x,y);
 }
 
@@ -49,7 +59,7 @@ Canvas.prototype.moveCanvas = function(x ,y)
 {
 	this.setLocation(x,y);
 	$(this.jstring).css({'top':this.x,"left":this.y});
-}
+};
 /**
 * Resizes the Canvas element.
 * @param h - the new height.
@@ -59,8 +69,9 @@ Canvas.prototype.resize = function(h, w)
 {
 	var current_height = $(this.jstring).height();
 	var current_width = $(this.jstring).width();
-	console.log(current_height);
-}
+	this.element.height = h;
+	this.element.width = w;
+};
 
 /**
 * Draws the canvas.
@@ -71,18 +82,68 @@ Canvas.prototype.resize = function(h, w)
 
 Canvas.prototype.draw = function(image)
 {
-	var canvas = $(jstring);
-	canvas.width = image.width;
-	canvas.height = image.height;
-	canvas.getContext("2d").drawImage(image, 0, 0);
+	//this.canvas.width = image.width;
+	//this.canvas.height = image.height;
+	var ctx = document.getElementById('ie-canvas').getContext("2d");
+	console.log(image);
+	//ctx.drawImage(image, 0, 0);
+};
+
+/**
+* Creates a square on the canvas
+* @param w - the width
+* @param h - the height
+* @param color - the color (might take hex values?)
+* @param offsetX - how much offset x you want
+* @param offsetY - how much offset y you want
+*
+*/
+Canvas.prototype.square =function(w,h, color, offsetX, offsetY)
+{
+	if (!offsetY)
+	{
+		// Set default to 0
+		var offsetY = 0;
+	}
+	if (!offsetX)
+	{
+		// Set default to zero
+		var offsetX = 0;
+	}
+	var c = this.element;
+	var ctx = c.getContext("2d");
+
+	ctx.beginPath();
+	// ctx.rect(offsetX,offsetY,width,height)
+	ctx.rect(offsetX, offsetY, w, h);
+	ctx.fillStyle = color;
+	ctx.fill();
 }
 
+/*
+* Converts canvas element to image object
+*
+*
+*/
 Canvas.prototype.convertCanvasToImage = function(canvas) 
 {	
 	var image = new Image();
 	image.src = canvas.toDataURL("image/jpg");
 	return image;
-}		
+};	
+
+/**
+* Turns image into usable object
+*
+*
+*/	
+Canvas.prototype.objectifyImage = function(i)
+{
+    var img_obj = new Image();
+	img_obj.src = i;
+	orig_image = img_obj;
+	return img_obj;
+};
 
 
 
@@ -96,16 +157,26 @@ Canvas.prototype.toString = function()
 	return 'Canvas("'+ this.id + '")';
 }
 
-
-
-
-// All actions after this are for when page is finished loading
-$(function(){
+	var button_resize_start = $('#ie-start-resize');
+	var button_resize_submit = $('#ie-button-resize');
+	var spec_resize_form = $('#ie-resize-spec-form');
 	console.log('image_editor_v2.js loaded!!');
 	var newCanvas = new Canvas('ie-canvas',0,0);
-	var varloc = newCanvas.getLocation();
-	console.log(varloc);
-	newCanvas.resize(500,1000);
+	button_resize_start.on('click', function(){
+		$(spec_resize_form).css({"display":"block"});
+	});
+	button_resize_submit.on('click', function(){
+		var new_height = $('#ie-resize-height').val();
+		var new_width = $('#ie-resize-width').val();
+
+		console.log(new_height);
+		console.log(new_width);
+		newCanvas.resize(new_height, new_width);
+		$(spec_resize_form).css({'display':'none'});
+
+	})
+	
+	
 
 })
 
--- template/view.image_editor.php
+++ template/view.image_editor.php
@@ -2,3 +2,18 @@
 
 
 <canvas id='ie-canvas'></canvas>
+
+
+
+<div id='ie-resize-spec-form'>
+	<label>Height:</label>
+	<input name='height' id='ie-resize-height' placeholder='height' type='number'/><br>
+	<label>Width:</label>
+	<input name='width' placeholder='height' id='ie-resize-width' type='number'/><br>
+	<button type='button' id='ie-button-resize'>Resize Canvas</button>
+</div>
+<div id='ie-controls-container'>
+	<div class='ie-control'>
+		<button id='ie-start-resize'>Resize</button>
+	</div>
+</div>
