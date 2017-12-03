a04848bd3900f5114a21f692893eff03d0b56b4f
shamelessApathy <bcm811@gmail.com>
2017-12-02 18:04:44 -0700

    successfully combined the shapes and drawing library of the tuorial with my own imagetools

--- template/footer.php
+++ template/footer.php
@@ -1,2 +1,2 @@
 <script src='js/jquery-3.2.1.js'></script>
-<script src='js/image_editor_v2.js'></script>
+<script src='js/shapes.js'></script>
--- template/view.image_editor.php
+++ template/view.image_editor.php
@@ -1,7 +1,7 @@
 <h1> You are in the image editor template</h1>
 
 
-<canvas id="canvas" width="400" height="300">
+<canvas id="canvas1" width="400" height="300">
     This text is displayed if your browser does not support HTML5 Canvas.
    </canvas>
 
@@ -9,7 +9,7 @@
 
 
 <div id='ie-controls-container'>
-	<div class='ie-control'>
+	<div class='ie-control' id='ie-resize'>
 		<button id='ie-start-resize'>Resize</button>
 		<div id='ie-resize-spec-form'>
 			<label>Height:</label>
@@ -18,5 +18,8 @@
 			<input name='width' placeholder='height' id='ie-resize-width' type='number'/><br>
 			<button type='button' id='ie-button-resize'>Resize Canvas</button>
 		</div>
+	</div>
+	<div class='ie-control'>
+		<button id='ie-draw-selector'>Draw Selector</button>
 	</div>
 </div> 
